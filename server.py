049530ebcb594de4632b4ffb28af10c7f2fdb94e
aahawash <58866085+aahawash@users.noreply.github.com>
2025-05-24 17:40:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def home():
     return "✅ Server is running"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))  # استخدم المنفذ الذي يرسله Render
+    app.run(host='0.0.0.0', port=port)
